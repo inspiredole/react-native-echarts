d169a393ab4b14d1bfe2e861ebf198598805fd6b
Inspire <34478749+inspiredole@users.noreply.github.com>
2020-09-14 20:09:30 +0800

    fix

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -35,7 +35,7 @@ export default class App extends Component {
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
           originWhitelist={['*']}
-          source={Platform.OS === 'ios' ? require('./index.html') : {uri: "file:///android_asset/html/index.html"}}
+          source={Platform.OS === 'ios' ? require('./index.html') : {uri: "file:///android_asset/html/tql.html"}}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
